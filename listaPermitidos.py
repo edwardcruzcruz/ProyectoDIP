8c4c07d72ed63f3389d05d4ea5b43e2a1b1ce137
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2018-11-11 07:23:55 -0500

    2da parte y final reconocimiento

---
+++ listaPermitidos.py
@@ -0,0 +1,11 @@
+class Politecnicos:
+    """ Lista de invitados a al labortorio CIDIS con el Mashi """
+
+    def __init__(self):
+        self.Invitados=['Edward Cruz']
+
+    def TuSiTuNo(self,EllosSi):        
+        if EllosSi in self.Invitados:
+            print('Bienvenido {}'.format(EllosSi))
+        else:
+            print('Lo siento {}, no estas en lista'.format(EllosSi))
